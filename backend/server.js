8c0a1a63c7d78e5a5c24fa9f1c49f165c4c7b9cd
Kistegez <83776780+Kistegez@users.noreply.github.com>
2023-01-22 20:13:18 +0100

    Feat - two endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let storedNumber = 0;
 
 app.get('/', (req, res) => {
     res.send('chalculator');
 });
+
+app.post('/store', (req, res) => {
+    storedNumber = req.body.number;
+    res.json({ message: 'Number stored successfully' });
+});
+
+app.get('/retrieve', (req, res) =>{
+    res.json({number: storedNumber});
+})
 
 app.listen(8080, () => {
     console.log('Server started on port 8080');
