3ed2cfff597f675114bc7f10ebe1b35e87ba37eb
Kistegez <83776780+Kistegez@users.noreply.github.com>
2023-01-22 20:51:45 +0100

    Refactor - sever more readable

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-const fs = require('fs');
+const { readNumber, writeNumber } = require('./fileHandler');
+const { isValidNumber } = require('./validator');
 
+const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('chalculator');
@@ -11,35 +12,22 @@ app.get('/', (req, res) => {
 
 app.post('/store', (req, res) => {
     const number = req.body.number;
-    if (isNaN(number) || number == null) {
+    if (!isValidNumber(number)) {
         res.status(400).json({ message: 'Invalid number' });
     } else {
-        fs.writeFileSync('storedNumber.txt', number);
-        res.json({message: 'Number stored successfully'});
+        writeNumber(number);
+        res.json({ message: 'Number stored successfully' });
     }
 });
 
-app.get('/retrieve', (req, res) =>{
-    try {
-        const storedNumber = fs.readFileSync('storedNumber.txt', 'utf-8');
-        if(!storedNumber){
-            res.json({ message: "No number stored yet"});
-        }else{
-            const number = parseInt(storedNumber, 10);
-            if (Number.isInteger(number)) {
-                res.json({ number });
-            } else {
-                res.status(400).json({ message: 'Invalid number' });
-            }
-        }
-    } catch (err) {
-        if (err.code === 'ENOENT') {
-            res.json({ message: "No number stored yet" });
-        } else {
-            res.json({ message: "Error occurred", error: err });
-        }
+app.get('/retrieve', (req, res) => {
+    const storedNumber = readNumber();
+    if (!storedNumber) {
+        res.json({ message: 'No number stored yet' });
+    } else {
+        res.json({ number: parseInt(storedNumber, 10) });
     }
-})
+});
 
 app.listen(8080, () => {
     console.log('Server started on port 8080');
