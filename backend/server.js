6741f31984877e217fbb1abe3b378a0c3d33a795
Kistegez <83776780+Kistegez@users.noreply.github.com>
2023-01-23 11:05:02 +0100

    Fix - number changed to double

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { readNumber, writeNumber } = require('./fileHandler');
-const { isValidNumber } = require('./validator');
+const { isValidDouble } = require('./validator');
 
 const app = express();
 app.use(cors({
